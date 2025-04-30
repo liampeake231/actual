ccb35eac3dc74b4755751aecbb2359430bc2bd90
liampeake231 <liam.gordon.peake@gmail.com>
2025-04-30 10:02:38 -0700

    l

--- app/classes/forms.py
+++ app/classes/forms.py
@@ -36,7 +36,7 @@ class ClinicForm(FlaskForm):
 
 
 class ReviewForm(FlaskForm):
-    name = SelectField('Thrift Store Name',choices=[("Goodwil","Out of the Closet"),("Thrift Town","Community Thrift"), ("American Cancer Society Discovery Shop", "Goodwill Store & Donation Center"), ("Salvation Army")])
+    name = SelectField('Thrift Store Name',choices=[("Goodwill","Out of the Closet"),("Thrift Town","Community Thrift"), ("American Cancer Society Discovery Shop", "Goodwill Store & Donation Center"), ("Salvation Army")])
     text = TextAreaField('Write your Review', validators=[DataRequired()])
     subject = SelectField('Experiences',choices=[("Price of Clothing", "Price of Clothing"), ("Customer Service","Customer Service"),("Selection","Selection"), ("Donation", "Donation"), ("Other","Other")])
     rating = IntegerField('Rate your experience: 0 is terrible, 10 is amazing', validators=[NumberRange(min=0,max=10, message="Enter a number between 0 and 10.")])
